0ac26bfea9bebebc784dc240fbc623e6ff20d372
bryce frazier and mike albers <bryce@>
2018-01-09 11:58:50 -0800

    set up skeleton for flashcard site

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#term-1").click(function() {
+    $("#term-1").addClass("hidden");
+    $("#def-1").removeClass("hidden");
+    $("#toggle-2").slideDown();
+    $("#toggle-3").slideDown();
+  });
+
+});
